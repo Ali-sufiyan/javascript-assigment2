4b5e9bf5867ba0c2bb6db047feb060cc3fcbaec5
Ali-sufiyan <115947671+Ali-sufiyan@users.noreply.github.com>
2022-11-03 17:45:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,149 @@
+// alert('hello how are you');
+// window.alert('hello how are you');
+
+
+///////////////////////////////////////////////////////
+
+
+// var firstname= prompt("enter first name");
+// var lastname= prompt("enter last name");
+// var subjects1= prompt ('enter subjest1');
+// var subjects2= prompt('enter subjest2');
+// var subjects3= prompt('enter subjest3');
+// var marks1= +prompt('subject1 mark');
+// var marks2= +prompt('subject2 mark');
+// var marks3= +prompt('subject3 mark');
+// var total= (marks1 + marks2 + marks3);
+// if(total > 250){
+//     console.log('you are pass in your exams');
+// }
+
+// else{
+//     console.log('fail');
+// }
+
+
+///////////////////////////////////////////////////////////
+
+
+
+// var firstname= prompt("enter first name");
+// var lastname= prompt("enter last name");
+// var subjects1= prompt ('enter subjest1');
+// var subjects2= prompt('enter subjest2');
+// var subjects3= prompt('enter subjest3');
+// var marks1= +prompt('subject1 mark');
+// var marks2= +prompt('subject2 mark');
+// var marks3= +prompt('subject3 mark');
+// var total= (marks1 + marks2 + marks3);
+// if(total > 250){
+//     console.log('you are pass in your exams');
+// }
+// else if(total >= 250 && total <= 250){
+//     console.log('you are pass in your exams');
+// }
+
+// else{
+//     console.log('fail');
+// }
+
+
+///////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+
+
+var firstname= prompt("enter first name");
+var lastname= prompt("enter last name");
+var subjects1= prompt ('enter subjest1');
+var subjects2= prompt('enter subjest2');
+var subjects3= prompt('enter subjest3');
+var marks1= +prompt('subject1 mark');
+var marks2= +prompt('subject2 mark');
+var marks3= +prompt('subject3 mark');
+var total= (marks1 + marks2 + marks3);
+if(total > 250){
+    console.log('you are pass in your exams');
+}
+else if(total >= 250 || total <= 250){
+    console.log('you are pass in your exams');
+}
+
+else{
+    console.log('fail');
+}
+
+
+/////////////////////////////////////
+///////////////////////////////////////////////////////////
+
+
+
+// var firstname= prompt("enter first name");
+// var lastname= prompt("enter last name");
+// var subjects1= prompt ('enter subjest1');
+// var subjects2= prompt('enter subjest2');
+// var subjects3= prompt('enter subjest3');
+// var marks1= +prompt('subject1 mark');
+// var marks2= +prompt('subject2 mark');
+// var marks3= +prompt('subject3 mark');
+// var total= (marks1 + marks2 + marks3);
+// if(total !== 250){
+//     console.log('you are pass in your exams');
+// }
+// // else if(total >= 250 && total <= 250){
+// //     console.log('you are pass in your exams');
+// // }
+
+// else{
+//     console.log('fail');
+// }
+
+
+///////////////////////////////////////
+///////////////////////////////////////////////////////////
+
+
+
+// var firstname= prompt("enter first name");
+// var lastname= prompt("enter last name");
+// var subjects1= prompt ('enter subjest1');
+// var subjects2= prompt('enter subjest2');
+// var subjects3= prompt('enter subjest3');
+// var marks1= +prompt('subject1 mark');
+// var marks2= +prompt('subject2 mark');
+// var marks3= +prompt('subject3 mark');
+// var total= (marks1 + marks2 + marks3);
+// if(total > 250){
+//     console.log('you are pass in your exams');
+// }
+// else if(total >= 250 && total <= 250){
+//     console.log('you are pass in your exams');
+// }
+
+// else{
+//     console.log('fail');
+// }
+
+
+///////////////////////////////////////
+
+
+
+// var firstname= prompt("enter first name");
+// var lastname= prompt("enter last name");
+// var subjects1= prompt ('enter subjest1');
+// var subjects2= prompt('enter subjest2');
+// var subjects3= prompt('enter subjest3');
+// var marks1= +prompt('subject1 mark');
+// var marks2= +prompt('subject2 mark');
+// var marks3= +prompt('subject3 mark');
+// var total= (marks1 + marks2 + marks3);
+// if(total > 250){
+//     console.log('you are pass in your exams');
+// }
+
+// else{
+//     console.log('fail');
+// }
+///////////////////////////////////
